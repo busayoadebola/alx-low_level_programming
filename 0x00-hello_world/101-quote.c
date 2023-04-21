87fb03845838ae0dcd7944bdc46a02398179e7fb
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-20 21:44:05 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to output without printf or puts
+ * Return: 1 (correct)
+ */
+int main(void)
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
+	return (1);
+}
