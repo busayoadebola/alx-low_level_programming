12c6e2f554bd1416475fd8645e9e66dea1aae023
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-21 13:34:45 -0700

    last

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/** main - A function to print the last number in n
+ * Return: 0 (correctt)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("Last digit of" n "is" 
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+For conditional and relational scripts
