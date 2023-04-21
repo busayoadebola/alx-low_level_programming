a935df1aa1d2103aae4e02449787bfddcd2c9fa9
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-20 19:43:21 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - A program to print a string with the printf function
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(str1);
+	return (0);
+}
