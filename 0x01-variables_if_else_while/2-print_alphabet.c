ceddfcb5e50262f1f2ae1167305bed49fa0fcf90
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-21 16:41:56 -0700

    alp

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program to write the alphabet in smal case
+ * Return: 0 (correct)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	return (0);
+}
