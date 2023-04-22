440e55bfbaef74149ac24dccd9a540e756499d8c
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-21 17:59:09 -0700

    combine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A function to print all combinations of single digit numerically
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+		if (n != 57)
+			putchar('.');
+			putchar(' ');
+	putchar('\n');
+	return (0);
+}
