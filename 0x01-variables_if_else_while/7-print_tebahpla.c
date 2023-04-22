c94b525ec74e3d8cfb16246352310d594762a84b
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-21 17:38:18 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program to write the alphabet in small case in reverse
+ * Return: 0 (correct)
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	for (letter = 'z'; letter <= 'a'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
