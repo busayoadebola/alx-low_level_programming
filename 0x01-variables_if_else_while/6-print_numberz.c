7fd663e753b1985fc083a1d4163c62a7025328fd
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-21 17:30:33 -0700

    comit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A function to print the singl digits in base 10
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
