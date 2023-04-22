35361be1709918dc89d55674f1fa8845a5c0c910
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-21 17:10:35 -0700

    omit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - A program to write the alphabet in small case minus q and e
+ * Return: 0 (correct)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != e && letter != q)
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
