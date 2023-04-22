b6968c7bf028b4577186a5d8a3c4799d20ef213a
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-21 17:50:20 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A function to print the numbers of base 16
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	for (n = 98; n < 103; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
