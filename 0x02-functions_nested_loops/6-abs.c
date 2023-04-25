3582ecf96a209ff4634bfdefa2a459f6ebb0c432
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-25 16:32:34 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - A function to print the absolute value of integers
+ * @int: is used to declare numerous variables
+ *
+ * Return: 0 correct
+ */
+
+int _abs(int b)
+{
+	int a;
+
+	a = abs(b);
+	_putchar(a);
+	return (0);
+}
