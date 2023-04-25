9a34142b412a4fb61821df5a2cce8981bda52260
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-25 13:00:27 -0700

    first func

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - A function to print small letters only
+ *
+ * Return: 0 (correct)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
 putchar();
+
+void print_alphabet(void){
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
