ac6e7aa3d9afd42c5e7bf599610eda0d3d54a04c
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-26 11:20:20 -0700

    correct

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,44 +8,40 @@
 
 void print_times_table(int n)
 {
-	if (n >=0 && n <=15)
+	if (n >= 0 && n <= 15)
 	{
-		int i, j, mult;
+		int i, j, m;
 
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0 ; j <= n; j++)
 			{
-				mult = i * j;
+				m = i * j;
 				if (j == 0)
 				{
-					_putchar(mult + '0');
-				}
-				else if (mult < 10 && j != 0)
+					_putchar(m + '0');
+				} else if (m < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(mult + '0');
-				}
-				else if (mult >= 10 && mult < 100)
+					_putchar(m + '0');
+				} else if (m >= 10 && m < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((mult / 10) + '0');
-					_putchar((mult % 10) + '0');
-				}
-				else if (mult >= 100)
+					_putchar((m / 10) + '0');
+					_putchar((m % 10) + '0');
+				} else if (m >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((mult / 100) + '0');
-					_putchar(((mult / 10) % 10) + '0');
-					_putchar((mult % 10) + '0');
+					_putchar((m / 100) + '0');
+					_putchar(((m / 10) % 10) + '0');
+					_putchar((m % 10) + '0');
 				}
-
 			}
 			_putchar('\n');
 		}
