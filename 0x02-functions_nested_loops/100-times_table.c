ffb282e6841c81b535fa60cf4a8c0820c46b9101
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-26 10:58:20 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,32 +8,39 @@
 
 void print_times_table(int n)
 {
-	int i, j, mult;
-
-	for (i = 0; i <= n; i++)
+	if (n ==0 && n <=15)
 	{
-		for (j = 0 ; j <= n; j++)
+		int i, j, mult;
+
+		for (i = 0; i <= n; i++)
 		{
-			mult = i * j;
-			if (j == 0)
+			for (j = 0 ; j <= n; j++)
 			{
-				_putchar(mult + '0');
+				mult = i * j;
+				if (j == 0)
+				{
+					_putchar(mult + '0');
+				}
+				else if (mult < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mult + '0');
+				}
+				else if (mult >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((mult / 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
 			}
-			else if (mult < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(mult + '0');
-			}
-			else if (mult >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((mult / 10) + '0');
-				_putchar((mult % 10) + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
+	}
+	else
+	{
+		_putchar(' ');
 	}
 }
