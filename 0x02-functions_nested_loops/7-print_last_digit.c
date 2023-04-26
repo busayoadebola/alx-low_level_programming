e062871631f1c23ceb87b59f12507b834c1af76c
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-25 17:09:15 -0700

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_last_digit - A function to print the absolute value of integers
+ * @b: is used to declare numerous variables
+ *
+ * Return: 0 correct
+ */
+
+int print_last_digit(int b)
+{
+	int n;
+
+	n = b % 10;
+	return (n);
+}
+
