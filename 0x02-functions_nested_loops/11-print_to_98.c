e299ab7108d3994ad5a26c47c7f010f20bb2f201
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-26 02:15:20 -0700

    natt

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,15 +18,15 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d\n", n);
 			}
 			n++;
 
 		}
 	}
-	else if (n > 98)
+	else if (n >= 98)
 	{
-		while (n > 98)
+		while (n >= 98)
 		{
 			if (n > 98)
 			{
@@ -34,7 +34,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d\n", n);
 			}
 			n--;
 		}
