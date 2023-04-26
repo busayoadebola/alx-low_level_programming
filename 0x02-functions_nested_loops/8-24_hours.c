0c44a557c1803257b4b6a0cc7aad1341bdca0d21
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-25 17:39:12 -0700

    add

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - A function to print minutes in a day
+ * Return: 0 (correct)
+ */
+void jack_bauer(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 24; n++)
+	{
+		for (m = 0; m <= 60; m++)
+		{
+			_putchar((n / 10) + 0);
+			_putchar((n % 10) + 0);
+			_putchar('.');
+			_putchar((m / 10) + 0);
+			_putchar((m % 10) + 0);
+		}
+	}
+}
