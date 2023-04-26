35eb12414bc8d881741d412990552e8bec0e4c93
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-26 01:13:30 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - to add two integers and displays the result
+ * @a: to contain the first integer
+ * @b: to contain the second integer
+ * Return: 0 correct
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
