bc83172c7774164f1c570a18d7cfee07a38f4e94
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-27 08:19:20 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - A function to check if a letter is in small case
+ * @c: letter to be checked
+ *
+ * Return: 1 if c is uppercase else return 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
