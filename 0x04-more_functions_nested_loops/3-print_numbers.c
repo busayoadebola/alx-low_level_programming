22c822e670e63fdbdc53a51390d5c13f75111171
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-27 08:39:17 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - A function to print numbers 1 to 9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
