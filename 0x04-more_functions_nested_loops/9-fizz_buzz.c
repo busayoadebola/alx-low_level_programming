f0292c09be7c86ec82e3ff02d3d25dcf4296f3f6
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-27 14:41:22 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - to print 1 to 100
+ *
+ * Return: 0 correct
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) != 0)
+		{
+			printf(" Fizz");
+		}
+		else if ((num % 5) == 0 && (num % 3) != 0)
+		{
+			printf(" Buzz");
+		}
+		else if ((num % 3) == 0 && (num % 5) == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (num == 1)
+		{
+			printf("%d", num);
+		}
+		else
+		{
+			printf(" %d", num);
+		}
+	}
+	printf('\n');
+
+	return (0);
+}
+
