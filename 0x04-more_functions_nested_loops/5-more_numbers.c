0dd5c970833043b1ced8abebf5508add3390d9f4
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-04-27 10:09:56 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,15 @@ void more_numbers(void)
 {
 	int num, i;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 1; num <= 10; num++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
 			{
-					_putchar('1');
-				_putchar(i % 10 + '0');
+				_putchar('1');
+			}
+			_putchar(i % 10 + '0');
 			}
 		}
 		_putchar('\n');
