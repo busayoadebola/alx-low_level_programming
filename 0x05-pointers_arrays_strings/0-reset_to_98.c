b35825202fea6afccd169c172000859ab035486c
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-02 09:31:28 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - to update the value of a pointer
+ * @n: parameter to be changed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - to print out the value to stdout
+ * @c: what to print
+ *
+ * Return: 0 on success
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
