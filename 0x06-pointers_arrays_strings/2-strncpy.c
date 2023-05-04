45bf99d3dcb5a6d31f5c297f80a74a0b2ce2a784
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-04 12:51:23 -0700

    copy2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncpy - to copy a string in a dest to another
+ * @dest: the destination to copy to
+ * @src: the place to copy from
+ * @n: amount to copy
+ *
+ * Return: dest the pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int str_len = 0;
+	int copy = 0;
+
+	while (*(src + str_len) != '\0')
+	{
+		str_len++;
+	}
+	while (copy < n && copy < str_len)
+	{
+		dest[copy] = src[copy];
+		copy++;
+	}
+	dest[str_len] = '\0';
+	return (dest);
+}
