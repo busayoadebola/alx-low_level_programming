bf2826f61dbf0da42be175cf47d10c4dc745e443
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-04 20:02:40 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - to print an array in reverse
+ * @a: array to reverse
+ * @n: array size
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+
+	for (int i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
