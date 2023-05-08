a78caf2f2b71e5a03bfcfff4019d1134756d831d
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-08 14:21:19 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,8 +2,22 @@
 
 /**
  * *_memset - to replace a no of bytes with a char
+ * @s: memory to fill
+ * @b: what to fill the memory with
+ * @n: the number of spaces to fill
+ *
+ * Return: s the filled memory
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	int i = 0;
+
+	while (n > 0)
+	{
+		s[i] = b;
+		n--;
+		i++;
+	}
+	return (s);
 }
