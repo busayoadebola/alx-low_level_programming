542b00a3bf1121b551d169a8d174fbb3f6a6a942
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-09 17:18:10 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - to assign the content of a pointer
+ * to a char
+ * @s: the pointer
+ * @to: the value
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
