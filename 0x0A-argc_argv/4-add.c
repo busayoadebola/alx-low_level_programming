b1a729e8134b5469e6d4d859070d5082c0a0fed1
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-16 08:48:41 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,46 +4,6 @@
 #include <string.h>
 
 /**
- * _atoi - to convert a string to integer
- * @s: the string to convert
- *
- * Return: num
- */
-
-int _atoi(char *s)
-{
-	int a, b, n, len, f, digit;
-
-	a = b = n = len = f = digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	for (; a < len && f == 0; a++)
-	{
-		if (s[a] == '-')
-			++b;
-
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			digit = s[a] - '0';
-			if (b % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			f = 0;
-		}
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
  * num_status - to check if a string is empty
  * @str: the string to check
  * Return: 0 correct
@@ -56,7 +16,7 @@ int num_status(char *str)
 	count = 0;
 	while (count < strlen(str))
 	{
-		if (isdigit(str[count]) != '\0')
+		if (!isdigit(str[count]))
 		{
 			return (0);
 		}
@@ -84,7 +44,7 @@ int main(int argc, char *argv[])
 	{
 		if (num_status(argv[i]))
 		{
-			type_change = _atoi(argv[i]);
+			type_change = atoi(argv[i]);
 			sum = type_change + 1;
 		}
 		else
