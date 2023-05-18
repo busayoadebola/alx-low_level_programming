44bacfb676639f60abe853808504252eaa95587d
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-18 15:07:09 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - to assign a space in memory
+ * using malloc
+ * @b: size of the malloc
+ * Return: str a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+
+	str = malloc(sizeof(unsigned int) * b);
+	if (str == NULL)
+		exit(98);
+
+	return (str);
+}
