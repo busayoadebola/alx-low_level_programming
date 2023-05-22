9bba9fadf323670a8dd6984a1659779c351e306d
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-22 14:34:55 -0700

    file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "0-object_like_macro.h"
+#define _FILE_
+
+/**
+ * main - to print the program file name
+ * Return: 0 correct
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
