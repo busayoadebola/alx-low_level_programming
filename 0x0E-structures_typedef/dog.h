c074610f494c2f2acae56e00e675e9c584c30558
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-22 07:54:52 -0700

    dog details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - Short description for dog
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* MAIN_H */
