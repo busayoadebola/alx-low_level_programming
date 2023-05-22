8f8690f5ffd1b3e135a7fe265a78679047ae0c7a
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-22 09:34:28 -0700

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - to print the values
+ * @d: the structure to print
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
