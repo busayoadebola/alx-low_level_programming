727d5e83d5f6a19ca980a97b661ea38b408b7a04
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-22 08:27:50 -0700

    initialization

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - to initialize a structure
+ * @d: the structure name
+ * @name: the first parameter
+ * @age: the second parameter
+ * @owner: the third parameter
+ * Return: 0 correct
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog d;
+
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+	return (0);
+}
+
