c6f80a50a86e3e531c51f066a901384047307f91
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-25 14:21:08 -0700

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - to sum all the arguements provided
+ * @n: the number of arguements
+ * Return: 0 if n is 0 or sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list apv;
+	int i, sum;
+
+	va_start(apv, n);
+	if (n == 0)
+		return (0);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(apv, int);
+
+	va_end(apv);
+	return (sum);
+}
