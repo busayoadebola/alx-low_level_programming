69b97acfcdfa3dc22945ddd34c57265b549f4c91
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-05-25 15:55:29 -0700

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - to print all the strings
+ * @separator: to seperate the strings
+ * @n: the number of arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	i = 0;
+	while (i < n)
+	{
+		const char *str = va_arg(ap, const char *);
+
+		printf("%s", str);
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
