ce00a8b2e5c9b0ebb8501a428e2f5f745b8b5232
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-06-02 08:59:42 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-section .data
-    msg db "Hello, Holberton",10
-
-section .text
-    global _start
-
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
-    syscall
-
-    mov rax, 60
-    mov rdi, 0
-    syscall
+lobal   main
+	extern    printf
+main:
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov   eax, 0
+	ret
+format: db `Hello, Holberton\n`,0
