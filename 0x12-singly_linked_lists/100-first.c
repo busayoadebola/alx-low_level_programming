338dd1bf4e2843547422b964c54dfa06006c2461
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-06-02 08:43:50 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void before_main(void) __attribute__ ((constructor));
+
+/**
+ * before_main - to print a sentence before the
+ * main function executes
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
