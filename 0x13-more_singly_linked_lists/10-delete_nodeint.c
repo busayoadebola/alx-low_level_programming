40956984db7a23748b754bd57b14790187ba5ddc
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-06-06 20:04:29 -0700

    delete a specific node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *current_node;
-	listint_t *tmp = *head;
+	listint_t *tmp = *head = NULL;
 
 	if (*head == NULL)
 		return (-1);
