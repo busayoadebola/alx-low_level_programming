e58c7e2a080e82cbc4242bb5262a267c4e3e18db
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-06-06 18:51:25 -0700

    changes in the null position

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current = head;
 
-	if (current == NULL)
-		return (NULL);
-
 	for (i = 0; i < index; i++)
 	{
 		current = current->next;
 	}
+	if (current == NULL)
+		return (NULL);
+
 	return (current);
 }
