cd84285fddfd577881adc5f6a45318eac0940145
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-06-07 13:39:07 -0700

    find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,3 +1,32 @@
 #include "lists.h"
 
+/**
+ * find_listint_loop - search for a loop in a linked list
+ * @head: list to search through
+ * Return: address of the node where the loop starts, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *normal = head;
+	listint_t *speed = head;
 
+	if (head == NULL)
+		return (NULL);
+
+	while (normal && speed && speed->next)
+	{
+		speed = speed->next->next;
+		normal = normal->next;
+		if (speed == normal)
+		{
+			normal = head;
+			while (normal != speed)
+			{
+				normal = normal->next;
+				speed = speed->next;
+			}
+			return (speed);
+		}
+	}
+	return (NULL);
+}
