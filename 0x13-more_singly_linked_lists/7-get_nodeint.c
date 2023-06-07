e0a91f1b388136d5bfa18cc694e9285bb007bc94
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-06-06 18:58:44 -0700

    new changes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current = head;
 
-	for (i = 0; i < index; i++)
+	if (current == NULL)
+		return (NULL);
+
+	for (i = 0; current != NULL && i < index; i++)
 	{
 		current = current->next;
 	}
-	if (current == NULL)
-		return (NULL);
 
 	return (current);
 }
