6967503fcbad5853752142b70475cfe4181bee9f
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-06-08 19:12:02 -0700

    binary to unsignedint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,11 +14,12 @@ unsigned int binary_to_uint(const char *b)
 	int c;
 	unsigned int _str_len;
 
+	if (b == NULL)
+		return (0);
+
 	_str_len = strlen(b);
 	for (c = _str_len - 1; c >= 0; c--)
 	{
-		if (b == NULL)
-			return (0);
 		if (b[c] != '0' && b[c] != '1')
 			return (0);
 		if (b[c] == '1')
