ba2a279743a994c3b1bd88ed360ad3da9d4c70e7
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-06-16 17:42:58 -0700

    adding text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,23 +11,27 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int open_file, write_file, len;
+	int open_file, write_file, len = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len = 0; text_content[len]; len++)
+			;
 	}
 
 	open_file = open(filename, O_WRONLY | O_APPEND);
-	write_file = write(open_file, text_content, len);
-
-	if (open_file == -1 || write_file == -1)
+	if (oopen_file == -1)
 		return (-1);
 
+	write_file = write(open_file, text_content, len);
+	if (write_file == -1)
+	{
+		close(open_file);
+		return (-1);
+	}
 	close(open_file);
 
 	return (1);
