feac3f8acb4927173783fe795795c0a24b67fdaa
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-08-03 14:44:35 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converting a binary number
+ * to unsigned int
+ * @b: the number to convert
+ * Return: the converted number or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int _str_len;
+
+	if (b == NULL)
+		return (0);
+
+	_str_len = strlen(b);
+	for (c = _str_len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			i = i + a;
+		}
+		a = a * 2;
+	}
+	return (i);
+}
