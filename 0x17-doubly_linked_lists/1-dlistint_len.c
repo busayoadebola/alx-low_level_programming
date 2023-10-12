9f77ea00bc35c2bb7687efb3f937312a3488ebe7
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-12 14:18:59 -0700

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - to print the number of contents of a linked list
+ * @h: the linked list to print
+ * Return: print the list and the number in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
