d432bdb24a1e2c2f630bec4eb6b6b3a1fd213484
Akinjiola Oreoluwa <akinoreoluwa@gmail.com>
2023-10-12 14:43:06 -0700

    task 3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * *add_dnodeint_end - to add a node to the end of a list
+ * @head: the node to add
+ * @n: the string to add
+ * Return: the position of the new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+{
+	dlistint_t *new_node;
+	dlistint_t *test = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (test->next)
+		test = test->next;
+
+	test->next = new_node;
+	return (new_node);
+}
